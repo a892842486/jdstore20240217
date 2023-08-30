e643d228bf85969c7fa137e31c17fc6f94b054e2
Bamu <a892842486@github.com>
2023-08-30 11:43:27 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,27 @@
 class ApplicationController < ActionController::Base
   #要有 admin 權限才能進入
   protect_from_forgery with: :exception
+  #定義找車 給車 動作 (current_cart / find_cart)
+  helper_method :current_cart
 
   def admin_required #要有 admin 權限才能進入
     if !current_user.admin?
       redirect_to "/", alert: "You are not admin"
     end
+  end
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+     cart = Cart.find_by(id: session[:cart_id])
+     if cart.blank?
+       cart = Cart.create
+     end
+     session[:cart_id] = cart.id
+     return cart
   end
 end
